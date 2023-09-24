a34b49151fdbbc3a7f6d7a9770f8a2cf21aeb589
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-09-24 15:36:58 +0300

    goit-js-hw-10-0.1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
+
+
+// axios.defaults.headers.common["x-api-key"] = "live_vgNBhtHrqSCNuFY5Yw3ynDa3Wwt0FCFFW6MIdRMU3Mwdbi99sKE7NevY4DclOQnH";
+
+// new SlimSelect({
+//     select: '#selectElement'
+//   })
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
